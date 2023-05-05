5a25746b38223720337a5d279618acd809ddc9ea
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2023-05-05 01:25:34 +0100

    Create main_main app and Add html css img files for the home/login/singup Pages

---
+++ main_home/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('' , views.home , name="home-page" ),
+    path('login/' , views.login , name="login-page" ),
+]
