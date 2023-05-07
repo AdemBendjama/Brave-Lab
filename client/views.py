ca9579e8875d48238ee0b6cb17e4e6e45755ecc2
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2023-05-08 00:30:51 +0100

    add error msg to login page

--- client/views.py
+++ client/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required , permission_required
 
 # Create your views here.
 
