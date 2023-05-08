6d69113aee6e9aeb70f13357ef6ae8b0c51b429c
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2023-05-08 10:08:50 +0100

    adjust form errors
    add regex validator for phone number input, adjust positioning and font-size for error messeges

--- main_home/forms.py
+++ main_home/forms.py
@@ -2,10 +2,15 @@ from django import forms
 from django.core.validators import RegexValidator
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+
+phone_regex = RegexValidator(
+    regex=r'^0[5-7][0-9]{8}$',
+    message='Please enter a valid phone number starting with 05, 06 or 07 and has 10 digits in total'
+)
                              
 class UserRegisterForm(UserCreationForm):
     
-    phone_number = forms.IntegerField(widget = forms.NumberInput(attrs={'placeholder': '+213 0000000000'}))
+    phone_number = forms.CharField(validators=[phone_regex],widget = forms.NumberInput(attrs={'placeholder': '+213 0000000000'}))
     gender = forms.ChoiceField(choices=((None, 'Choose Here :'),('Male', 'Male'), ('Female', 'Female')))
     address = forms.CharField(max_length=50,widget = forms.TextInput(attrs={'placeholder': 'Setif, El Eulma'}))
     policy = forms.BooleanField()
