e9893f77e6e27ea69ba18a600e18c0be3f79cead
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2023-05-13 15:40:11 +0100

    add privacy policy to register page

--- main_home/urls.py
+++ main_home/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
     path('' , views.home , name="home" ),
     path('register/' , views.register , name="register" ),
+    path('policy/', TemplateView.as_view(template_name='main_home/policy.html'), name='policy'),
     path('login/' , views.login_view , name="login" ),
     path('logout/' ,auth_views.LogoutView.as_view(template_name="main_home/logout.html")  , name="logout" ),
     path('profile/',views.profile_update,name="profile_update"),
