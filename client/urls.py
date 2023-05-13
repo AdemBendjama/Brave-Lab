aa3cd00317829b9dc645c9e7ca4ddfb5f3f45466
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2023-05-13 10:06:33 +0100

    update pages of profile drop down menue, quality of life changes

--- client/urls.py
+++ client/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("",views.client_home,name="client"),
+    path("help/",views.client_help,name="profile_help"),
+    path("contact/",views.client_contact,name="profile_contact"),
+    path("policy/",views.client_policy,name="profile_policy"),
 ]
--- client/views.py
+++ client/views.py
@@ -9,3 +9,21 @@ def client_home(request):
     
     return render(request,'client/client.html')
 
+@login_required
+@permission_required('client.view_client', raise_exception=True)
+def client_help(request):
+    
+    return render(request,'client/profile/help.html')
+
+@login_required
+@permission_required('client.view_client', raise_exception=True)
+def client_contact(request):
+    
+    return render(request,'client/profile/contact.html')
+
+@login_required
+@permission_required('client.view_client', raise_exception=True)
+def client_policy(request):
+    
+    return render(request,'client/profile/policy.html')
+
