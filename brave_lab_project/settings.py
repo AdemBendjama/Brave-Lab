9f016ddace55c0711fb8d9f9f672bc3353ad0ad4
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2023-06-16 22:01:17 +0100

    add req file

--- brave_lab_project/settings.py
+++ brave_lab_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qdw6s03ob(#w@ml#zsd=vef56q)#g=&hv90od0wc*p&b1!o+2)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['brave-lab.azurewebsites.net']
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,6 +128,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = "/media/"
