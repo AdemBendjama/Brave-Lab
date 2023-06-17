c219b261174eb3ec025392a2b0e4d083cafbfb6e
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2023-06-17 04:33:40 +0100

    add some security to secret key

--- brave_lab_project/settings.py
+++ brave_lab_project/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qdw6s03ob(#w@ml#zsd=vef56q)#g=&hv90od0wc*p&b1!o+2)'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
@@ -30,6 +32,7 @@ SECRET_KEY = 'django-insecure-qdw6s03ob(#w@ml#zsd=vef56q)#g=&hv90od0wc*p&b1!o+2)
 # Remember to turn DEBUG off in Deployment
 ALLOWED_HOSTS = ['brave-lab.azurewebsites.net','localhost','127.0.0.1']
 DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
 # Application definition
