26005514e21b518ddce158266482523136eb8ba9
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2023-06-27 18:01:20 +0100

    make online payment a bit more secure

--- brave_lab_project/settings.py
+++ brave_lab_project/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = "lreksgnldkfjgndigksndfgkmj6vs5dfv16ds"
+# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
